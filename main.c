6a3485f7b3206368e5c623df2a2d8b9f6f70a518
Adam Pacek <61830085+AdasPa@users.noreply.github.com>
2025-11-06 11:55:15 +0100

    first upload

---
+++ main.c
@@ -0,0 +1,53 @@
+/**
+ * @file main.c
+ * @author adas
+ * @date 2025-11-05
+ * @brief Main function
+ */
+
+#include <util/delay.h>
+#include "usart.h"
+
+
+void AC_init(){
+    // Set pin PD2 (port D, pin 2) as an input
+    PORTD.DIRCLR = PIN2_bm;
+    // Disable digital input buffer and pull-up resistor for pin PD2
+    PORTD.PIN2CTRL = PORT_ISC_INPUT_DISABLE_gc;
+    
+    AC0.MUXCTRL = AC_MUXPOS_AINP2_gc | AC_MUXNEG_DACREF_gc;
+    AC0.DACREF = 25; //according to formula
+    AC0.CTRLA = AC_ENABLE_bm | AC_RUNSTDBY_bm;
+}
+
+
+void VREF_init(void) {
+    VREF.ACREF = VREF_REFSEL_1V024_gc;
+}
+
+
+bool AC_above_threshold() {
+    // CMPSTATE - 1 = above , 0 = below
+
+    if (AC0.STATUS & AC_CMPSTATE_bm) 
+    {
+        return true;  // Above threshold - BRIGHT
+    } 
+    else 
+    {
+        return false; // Below threshold - DARK
+    }
+}
+
+
+int main(void) {
+    USART3_Init();
+    
+    USART3_SendChar('s');
+
+    AC_init();
+    VREF_init();
+
+    
+    return 0;
+}
