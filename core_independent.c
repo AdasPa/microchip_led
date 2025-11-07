23d2903c2ae429b750090b740bc79c76e7e9e419
Adam Pacek <61830085+AdasPa@users.noreply.github.com>
2025-11-07 14:25:51 +0100

    Update core_independent.c

--- core_independent.c
+++ core_independent.c
@@ -6,7 +6,7 @@ void AC_init(){
     PORTD.PIN2CTRL = PORT_ISC_INPUT_DISABLE_gc;
     AC0.MUXCTRL = AC_MUXPOS_AINP0_gc | AC_MUXNEG_DACREF_gc;
     AC0.CTRLA = AC_ENABLE_bm | AC_RUNSTDBY_bm;
-    AC0.DACREF = 25;
+    AC0.DACREF = 25; // according to formula, 0.1V
 }
 
 void VREF_init(void) {
@@ -14,10 +14,10 @@ void VREF_init(void) {
 }
 
 void LED_init() {
-    PORTA.DIRSET = PIN2_bm;
-    PORTA.OUTSET = PIN2_bm;
-    EVSYS.USEREVSYSEVOUTA = EVSYS_USER_CHANNEL0_gc;
-    EVSYS.CHANNEL0 = EVSYS_CHANNEL0_AC0_OUT_gc;    
+    PORTA.DIRSET = PIN2_bm; //PA2 - output
+    PORTA.OUTSET = PIN2_bm; //start tith turned off
+    EVSYS.USEREVSYSEVOUTA = EVSYS_USER_CHANNEL0_gc; //event user: channel 0 -> event out (PA2)
+    EVSYS.CHANNEL0 = EVSYS_CHANNEL0_AC0_OUT_gc;    //event generator: ac0 output -> channel 0
 }
 
 void sleep_init(void) {
